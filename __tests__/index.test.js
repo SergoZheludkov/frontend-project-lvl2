ae876197003a13626a1d2e516571a5244274182d
SergoZheludkov <58815292+SergoZheludkov@users.noreply.github.com>
2020-03-24 20:10:31 +0300

    Update index.test.js
    refactoring test for codeclimate

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,43 +9,28 @@ const resultJsonLink = `${__dirname}/fixtures/resultJson.txt`;
 
 const jsonLink1 = `${__dirname}/fixtures/before.json`;
 const jsonLink2 = `${__dirname}/fixtures/after.json`;
+
 const yamlLink1 = `${__dirname}/fixtures/before.yml`;
 const yamlLink2 = `${__dirname}/fixtures/after.yml`;
+
 const iniLink1 = `${__dirname}/fixtures/before.ini`;
 const iniLink2 = `${__dirname}/fixtures/after.ini`;
 
-describe('Tree', () => {
-  test('json', () => {
+
+describe('AllFormaters', () => {
+  test('Tree', () => {
     expect(showMeTheDifferences(jsonLink1, jsonLink2, 'tree')).toEqual(getResult(resultTreeLink));
-  });
-  test('yaml', () => {
     expect(showMeTheDifferences(yamlLink1, yamlLink2, 'tree')).toEqual(getResult(resultTreeLink));
-  });
-  test('ini', () => {
     expect(showMeTheDifferences(iniLink1, iniLink2, 'tree')).toEqual(getResult(resultTreeLink));
   });
-});
-
-describe('Plain', () => {
-  test('json', () => {
+  test('Plain', () => {
     expect(showMeTheDifferences(jsonLink1, jsonLink2, 'plain')).toEqual(getResult(resultPlainLink));
-  });
-  test('yaml', () => {
     expect(showMeTheDifferences(yamlLink1, yamlLink2, 'plain')).toEqual(getResult(resultPlainLink));
-  });
-  test('ini', () => {
     expect(showMeTheDifferences(iniLink1, iniLink2, 'plain')).toEqual(getResult(resultPlainLink));
   });
-});
-
-describe('JSON', () => {
-  test('json', () => {
+  test('JSON', () => {
     expect(showMeTheDifferences(jsonLink1, jsonLink2, 'json')).toEqual(getResult(resultJsonLink));
-  });
-  test('yaml', () => {
     expect(showMeTheDifferences(yamlLink1, yamlLink2, 'json')).toEqual(getResult(resultJsonLink));
-  });
-  test('ini', () => {
     expect(showMeTheDifferences(iniLink1, iniLink2, 'json')).toEqual(getResult(resultJsonLink));
   });
 });
